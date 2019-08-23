be4fb17c598fa1b729ef1b3bc8ee828b21074f00
roaz1801 <54395470+roaz1801@users.noreply.github.com>
2019-08-23 18:09:43 +0200

    Started on the project, tried making matrix. Not complete

--- project1.cpp
+++ project1.cpp
@@ -1,1 +1,23 @@
-//Hello world
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+double a[3][3];
+
+
+
+int main ()
+{
+  int i = 0;
+  int j = 1;
+  int n = 3;
+  for(i=0;i<n;i++){
+    for(j=1; j<n; j++){
+      a[i][j] += x[i]*y[j];
+    };
+  }
+printf(a);
+}
