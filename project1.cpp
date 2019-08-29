b1b7fb1e69cc54e0625a9a61c799a4b4ef0b4125
roaz1801 <54395470+roaz1801@users.noreply.github.com>
2019-08-29 19:10:09 +0200

    Added forward/backward sub to test
    Added forward/backward sub to test. Added it to project1 too. Also added a forward/backward sub to test with e=1 and optimized to the computer doesn't have to do the math.

--- project1.cpp
+++ project1.cpp
@@ -2,34 +2,25 @@
 #include <cmath>
 #include <string>
 
-
-
 using namespace std;
-
-#define food 5
-#define age 3
-
-
-int foo[food][age];
-int n,m;
 
 int main(){
     //for(int i=1;i<4;i++){
-    int n = pow(10,1);
+    //int n = pow(10,1);
     //cout << n;
     //}
 
-    //double u = 1-(1-exp(-10))*x-e**(-10*x)
+    /*//double u = 1-(1-exp(-10))*x-e**(-10*x)
     double h = 1/(n+1);
     for(int i=0;i>n;i++){
     double a[i] = -1/h**2;
      cout << a[i];
-    }
-   
+    }*/
+    
 
-/*
+
     //Forward substitution
-    btemp = b[1]
+   /* btemp[1] = b[1]
     u[1] = f[1]/btemp;
     for(i=2; i<=n; i++){
         temp[i] = c[i-1]/btemp;
@@ -42,13 +33,5 @@ int main(){
         u[i] -= temp[+1]*u[i+1]; 
     }
 */
-    
-
-    /*for(n=0;n<food;n++){
-        for(m=0; m<age; m++){
-            foo[n][m]=(n+1)*(m+1);
-        }
-    }
-    cout << foo[2][1];*/
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,9 +1,56 @@
-#include "stdafx.h"
-#include <iostream> 
+#include <iostream>
+#include <cmath>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-	cout << "Hello World! ";
-	return 0;
+int main(){	
+	double n = 1000;
+	double h = 1/(n+1);
+	double e = -1.0;
+
+	int w = 1;
+	double b[w];
+
+/*
+	//Making arrays	
+	double b[n];
+	double b_temp[n];
+	double f[n];
+	double f_temp[n];
+	double u[n];
+ 
+	//Forward sub
+	b_temp = b[1];
+	for(int i=2;i<=n-1;i++){
+		b_temp[i] = (b[i]-e[i-1]**2)/b_temp[i-1];
+		f[i] = (f[i]-f_temp[i-1]*e[i-1])/b_temp[i-1];
+	}
+
+	//Backward sub
+	u[n-1] = f[n-1]/b_temp[n-1];
+	for(int i=n-1; i>=1; i--){
+		u[i] = (f[i]**2-e[i]*u[i+1])/b_temp[i];
+	}
+
+
+	//Forward and backward sub with e=1
+
+	//Forward sub
+	b_temp = b[1];
+	for(int i=2;i<=n-1;i++){
+		b_temp[i] = b[i]/b_temp[i-1];
+		f[i] = (f[i]+f_temp[i-1])/b_temp[i-1];
+	}
+
+	//Backward sub
+	u[n-1] = f[n-1]/b_temp[n-1];
+	for(int i=n-1; i >= 1; i--){
+		u[i] = (f[i]**2+u[i+1])/b_temp[i];
+	}
+
+*/
+
+
+    return 0;
 }
