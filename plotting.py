173158257ad94b88665c56ec9caa1d9b189ce5f4
roaz1801 <54395470+roaz1801@users.noreply.github.com>
2019-08-31 16:54:02 +0200

    Program for plotting
    Made a program that reads from the file made by the program and then plots the exact and numerical values.

---
+++ plotting.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+exact_val = []
+num_val = []
+
+with open("hei.txt","r") as infile:
+    for i in infile:
+        data = i.split()
+        e_val,n_val = data[0].split("[]")[0],data[1].split("[]")[0]
+        exact_val.append(float(e_val))
+        num_val.append(float(n_val))
+
+
+
+
+plt.plot(exact_val,label="Exact value")
+plt.plot(num_val, label="Numerical value")
+plt.legend()
+plt.show()
+
+
+#print(exact_val)
